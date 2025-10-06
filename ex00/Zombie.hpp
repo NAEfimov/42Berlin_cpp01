d87a19c5a33f085d83e58b47dab0179326bcaa81
nefimov <nefimov@student.42berlin.de>
2025-10-06 17:16:37 +0200

    change private and public blocks in Zombie class declaration

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:44:26 by nefimov           #+#    #+#             */
-/*   Updated: 2025/10/06 16:55:37 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/10/06 17:15:38 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 # include <string>
 
 class Zombie {
+  public:
+	Zombie(std::string name);
+	~Zombie(void);
+	
+	void	announce(void);
+
   private:
 	Zombie(void);
 	
 	std::string	name;
-	
-  public:
-	Zombie(std::string name);
-	~Zombie(void);
-  
-  	void	announce(void);
 };
 
 // Zombie*	newZombie(std::string name);
